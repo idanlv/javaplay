46b74dc5b29f9369ad9fb2e66253ed9146217dca
Maya <maiki89g@gmail.com>
2016-08-27 23:14:30 +0300

    Following CR, changed keys.json structure

--- src/javaplay/utils/Keys.java
+++ src/javaplay/utils/Keys.java
@@ -3,6 +3,11 @@ package javaplay.utils;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -10,56 +15,106 @@ import org.json.simple.parser.ParseException;
 
 
 public class Keys {
+	private static Logger _logger = Logger.getLogger(Keys.class.getName()); 
+	
 	private static String _environment = null;
 	private static JSONObject _keys = null; 
+	private static final String DEFAULT_KEYS_FILE = "keys.json";
+	private static final String DEFAULT_ENVIRONMENT = "local";
+	private static final String JAVAPLAY_ENV = "JAVAPLAY_ENV";
+	private static final String JAVAPLAY_KEYS_FILE = "JAVAPLAY_KEYS_FILE";
 	
 	/***
-	 * Retrieves key's value
+	 * Retrieves key's String value
 	 * @param key Key to retrieve
 	 * @return value
 	 * @throws FileNotFoundException in case keys file wasn't found
 	 * @throws IOException in case can't read keys file
-	 * @throws ParseException in case keys file isn't a valid json file
+	 * @throws ParseException in case keys file isn't a valid JSON file
 	 */
-	public static String get(String key) throws FileNotFoundException, IOException, ParseException {
+	public static String getString(String key) throws FileNotFoundException, IOException, ParseException {
+		Object obj = get(key);
+		
+		if (obj instanceof String) {
+			return obj.toString();
+		} else {
+			throw new IllegalArgumentException(String.format("Key '%s' is not a String object", key));
+		}
+	}
+
+	/***
+	 * Retrieves key's Map value
+	 * @param key Key to retrieve
+	 * @return value
+	 * @throws FileNotFoundException in case keys file wasn't found
+	 * @throws IOException in case can't read keys file
+	 * @throws ParseException in case keys file isn't a valid JSON file
+	 */
+
+	public static Map<String, String> getMap(String key) throws FileNotFoundException, IOException, ParseException {
+		Object obj = get(key);
+		
+		if (obj instanceof JSONObject) {
+			JSONObject valueObject = (JSONObject)obj;
+			
+		    Map<String, String> map = new HashMap<String, String>();
+
+		    Set keysSet = valueObject.keySet();
+		    
+		    for (Object item : keysSet) {		    	
+		    	String innerKey = item.toString();
+		        Object value = valueObject.get(innerKey);
+
+		        map.put(innerKey, value.toString());
+		    }
+		    
+		    return map;
+			
+		} else {
+			throw new IllegalArgumentException(String.format("Key '%s' is not a JSONObject object", key));
+		}
+	}
+	
+	private static Object get(String key) throws FileNotFoundException, IOException, ParseException {
+		// TODO: Add start configuration that does this
 		if (_environment == null) {
 			loadEnvironment();
 			loadKeysFile();
 		}
 		
 		if (_keys.containsKey(_environment)) {
-			JSONObject envKey = (JSONObject)_keys.get(_environment);
+			JSONObject keysInEnvironment = (JSONObject)_keys.get(_environment);
 			
-			if (envKey.containsKey(key)) {
-				return envKey.get(key).toString();		
+			if (keysInEnvironment.containsKey(key)) {
+				return keysInEnvironment.get(key);
 			} else {
 				throw new IllegalArgumentException(String.format("Key %s does not exist in keys file", key));
 			}			
 		} else {
 			throw new IllegalArgumentException(String.format("Key %s does not exist in keys file", key));	
 		}
+
 	}
 	
 	private static void loadKeysFile() throws FileNotFoundException, IOException, ParseException {
-		String value = System.getenv("JAVAPLAY_KEYS_FILE");
+		String value = System.getenv(JAVAPLAY_KEYS_FILE);
         
-        if (value == null) {
-        	value = "keys.json";
-        } 
+		if (value == null) {
+			_logger.log(Level.WARNING, "Could not find environment variable '%s', using default instead", JAVAPLAY_KEYS_FILE);
+			value = DEFAULT_KEYS_FILE;
+		} 
         
-        JSONParser parser = new JSONParser();
+		JSONParser parser = new JSONParser();
         
-        Object obj = parser.parse(new FileReader(value));
-        _keys = (JSONObject)obj;
+		_keys = (JSONObject)parser.parse(new FileReader(value));
 	}
 	
 	private static void loadEnvironment() {
-        String value = System.getenv("JAVAPLAY_ENV");
+		_environment = System.getenv(JAVAPLAY_ENV);
         
-        if (value == null) {
-        	_environment = "local";
-        } else {
-        	_environment = value;
-        }
+		if (_environment == null) {
+			_logger.log(Level.WARNING, "Could not find environment variable '%s', using default instead", JAVAPLAY_ENV);
+			_environment = DEFAULT_ENVIRONMENT;
+		}
 	}
 }
