c38f4ee7b946f96bda3895e5678bfb00d31976ff
Maya <maiki89g@gmail.com>
2016-09-03 16:13:48 +0300

    Fixed db usage

--- Rest/src/javaplay/businesslogic/User.java
+++ Rest/src/javaplay/businesslogic/User.java
@@ -22,34 +22,26 @@ public class User {
 		DBHandler db = new DBHandler(DBHandler.DB_DEFAULT_HOST, DBHandler.DB_DEFAULT_PORT,
 				DBHandler.DB_DEFAULT_NAME, DBHandler.DB_DEFAULT_USER, DBHandler.DB_DEFAULT_PASSWORD);
 		
-		try {
-			db.openDBConnection();
-			
-			// Checks if another user exists with the same email
-			String email_query = 
-					String.format("SELECT COUNT(*) AS rowCount FROM USERS WHERE email = '%s'", credentials.getEmail());
-			
-			ResultSet result = db.exceute(email_query);
-			result.next();
-	
-			boolean email_exists = (result.getInt("rowCount") == 1); 
-			
-			if (email_exists) {
-				_logger.log(Level.INFO, "User email already exists in database");
-				return false;
-			}
-			
-			// Saves user's credentials in database
-			String signup_insert = String.format("INSERT INTO USERS (EMAIL, PASSWORD, USER_NAME) VALUES ('%s', '%s', '%s')", 
-					credentials.getEmail(), credentials.getPassword(), credentials.getUsername());
+		// Checks if another user exists with the same email
+		String email_query = 
+				String.format("SELECT COUNT(*) AS rowCount FROM USERS WHERE email = '%s'", credentials.getEmail());
 		
-			db.executeUpdate(signup_insert);
-			db.closeDBConnection();
-			
-			return true;
+		ResultSet result = db.exceute(email_query);
+		result.next();
+
+		boolean email_exists = (result.getInt("rowCount") == 1); 
 		
-		} finally {
-			db.closeDBConnection();
+		if (email_exists) {
+			_logger.log(Level.INFO, "User email already exists in database");
+			return false;
 		}
+		
+		// Saves user's credentials in database
+		String signup_insert = String.format("INSERT INTO USERS (EMAIL, PASSWORD, USER_NAME) VALUES ('%s', '%s', '%s')", 
+				credentials.getEmail(), credentials.getPassword(), credentials.getUsername());
+	
+		db.executeUpdate(signup_insert);
+		
+		return true;
 	}
 }
