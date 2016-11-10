55d2ce1629761abdf86f4f91bf67d694ae01ab1d
Maya <maiki89g@gmail.com>
2016-11-10 21:02:52 +0200

    Empty project

--- Rest/src/javaplay/resources/HealthResource.java
+++ Rest/src/javaplay/resources/HealthResource.java
@@ -1,14 +1,14 @@
-package javaplay.ws;
+package javaplay.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/health")
-public class Health {
+public class HealthResource {
 
 	@GET
 	public Response getIsAlive() {
-		return Response.status(200).entity("Java Play is Alive!").build();
+        return Response.status(200).entity("Java Play is Alive!").build();
 	}
 }
